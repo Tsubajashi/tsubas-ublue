b8e46cae6df7ce2b1b44548c43b1753921fb284a
Jan Jonas Willumeit <tsubajashi@protonmail.com>
2023-10-01 20:28:59 +0200

    feat: add more builds, cleanup

--- modules/bore-kernel/bore-kernel.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -oue pipefail
-
-# Installing the Bore EEVDF RT Kernel
-echo "Installing Custom Kernel (Bore)"
-rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos-bore-eevdf-rt
